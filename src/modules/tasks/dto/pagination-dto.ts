f7a3a758bb6c9ad7563acced04203946f5e5763d
FlavioMontoril <flavio.montorilll@gmail.com>
2026-04-14 14:29:48 -0300

    fix/adjusted prisma version to 5, dto pagination, prisma-service and included TaskRepository in providers>Module:TaskModule

--- src/modules/tasks/dto/pagination-dto.ts
+++ src/modules/tasks/dto/pagination-dto.ts
@@ -1,4 +1,19 @@
+// export class PaginationDto {
+//   page?: number = 1;
+//   limit?: number = 10;
+// }
+
+import { IsOptional, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class PaginationDto {
-  page?: number = 1;
-  limit?: number = 10;
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  page: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  limit: number = 10;
 }
